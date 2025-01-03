a80e16c5901fb72620948f5b65873519fbc21003
Adam Joseph Mather <adamjosephmather@gmail.com>
2025-01-02 23:32:51 -0700

    Preparing for V8.5.2 release

--- build/CodeWizard/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_CodeWizard_FILE C:/Users/adamj/OneDrive/Documents/QtProjects/CodeWizard/build/Desktop_Qt_6_7_1_MSVC2019_64bit-Release/CodeWizard.exe)
-set(__QT_DEPLOY_TARGET_CodeWizard_RUNTIME_DLLS C:/Qt/6.7.1/msvc2019_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6Core.dll)
