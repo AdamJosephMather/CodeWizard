a94e2083d7175e38ee143de755f1797249a7f8c5
Adam Joseph Mather <adamjosephmather@gmail.com>
2025-03-03 16:53:01 -0700

    Linux build for 8.9.5

--- build/linuxbuild/update-deps.sh
+++ build/linuxbuild/update-deps.sh
@@ -2,15 +2,15 @@
 # save as update-deps.sh
 
 # Copy your application files
-cp ../build-CodeWizard-Desktop-Release/CodeWizard codewizard/usr/local/bin/
-cp codewizard.desktop codewizard/usr/share/applications/
-cp codewizard.png codewizard/usr/share/icons/hicolor/256x256/apps/
+# cp ../build-CodeWizard-Desktop-Release/CodeWizard codewizard/usr/local/bin/
+# cp codewizard.desktop codewizard/usr/share/applications/
+# cp codewizard.png codewizard/usr/share/icons/hicolor/256x256/apps/
 
 # Generate dependencies
-DEPS=$(dpkg-shlibdeps -O codewizard/usr/local/bin/CodeWizard 2>/dev/null | sed 's/shlibs:Depends=//')
+# DEPS=$(dpkg-shlibdeps -O codewizard/usr/local/bin/CodeWizard 2>/dev/null | sed 's/shlibs:Depends=//')
 
 # Update control file with dependencies
-sed -i "/^Description:/i Depends: ${DEPS}" codewizard/DEBIAN/control
+# sed -i "/^Description:/i Depends: ${DEPS}" codewizard/DEBIAN/control
 
 # Build the package
 dpkg-deb --build codewizard
